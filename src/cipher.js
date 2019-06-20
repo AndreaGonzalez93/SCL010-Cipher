7e3ff5798f966c12cc437773a89ff83740032d60
Andrea Gonzalez <gogoz.gonzalez@gmail.com>
2019-06-20 10:53:57 -0400

    agregado de if

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,56 @@
 window.cipher = {
-  encode: (input, offset) => {
+    encode: (input, offset) => {
       let palabraCifrada = '';
 
       for (let i = 0; i < input.length; i++) {
-          let posicionInicialLetra = input[i].charCodeAt(0);
+        let posicionInicialLetra = input[i].charCodeAt(0);
+
+        if (posicionInicialLetra >= 65 && posicionInicialLetra <= 90) {
           let posicionFinalLetra = (posicionInicialLetra - 65 + offset) % 26 + 65;
           let letraCifrada = String.fromCharCode(posicionFinalLetra);
 
           palabraCifrada = palabraCifrada + letraCifrada;
-      }
-      return palabraCifrada;
-  },
-  decode: (input, offset) => {
-      let palabraDesCifrada = '';
 
-      for (let i = 0; i < input.length; i++) {
+        } else if (posicionInicialLetra >= 97 && posicionInicialLetra <= 122) {
+            let posicionFinalLetraM = (posicionInicialLetra - 97 + offset)% 26 +97 ;
+            let letraCifradaM = String.fromCharCode(posicionFinalLetraM);
+            palabraCifrada = palabraCifrada + letraCifradaM ;
+        }
+         else if (posicionInicialLetra >=48 && posicionInicialLetra <=57 ) {
+             let posicionFinalNumber = (posicionInicialLetra-48+offset) %10 +48;
+             let numbercipher = String.fromCharCode(posicionFinalNumber);
+             palabraCifrada = palabraCifrada + numbercipher ;
+         }
+
+        }
+        return palabraCifrada;
+      },
+
+      decode: (input, offset) => {
+        let palabraDesCifrada = '';
+
+        for (let i = 0; i < input.length; i++) {
           let posicionInicialLetra = input[i].charCodeAt(0);
+          
+        if (posicionInicialLetra >= 65 && posicionInicialLetra <= 90){
           let posicionFinalLetra = (posicionInicialLetra - 13 - offset) % 26 + 65;
           let letraCifrada = String.fromCharCode(posicionFinalLetra);
 
           palabraDesCifrada = palabraDesCifrada + letraCifrada;
+        }
+         else if (posicionInicialLetra >= 97 && posicionInicialLetra <= 122) {
+            let posicionFinalLetraM = (posicionInicialLetra - 45 - offset)% 26 +97 ;
+            let letraCifradaM = String.fromCharCode(posicionFinalLetraM);
+            palabraDesCifrada = palabraDesCifrada + letraCifradaM ;
+         }
+        else if (posicionInicialLetra >=48 && posicionInicialLetra <=57 ) {
+            let posicionFinalNumber = (posicionInicialLetra- 8 -offset) %10 +48;
+            let numbercipher = String.fromCharCode(posicionFinalNumber);
+            palabraDesCifrada = palabraDesCifrada + numbercipher ;
+
+
+        }
+        }
+        return palabraDesCifrada;
       }
-      return palabraDesCifrada;
-  }
-};
+    };
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,10 +13,16 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
       assert.equal(window.cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33),"HIJKLMNOPQRSTUVWXYZABCDEFG" )
     });
+    it ('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+      assert.equal(window.cipher.encode("abcdefghijklmnopqrstuvwxyz", 33),"hijklmnopqrstuvwxyzabcdefg" ) 
+      });
+    it ('debería retornar "3456789012" para "0123456789" con offset 33', () => {
+      assert.equal(window.cipher.encode("0123456789", 33),"3456789012" ) 
+    });
   });
 
   describe('cipher.decode', () => {
-
+    3456789012
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
@@ -24,6 +30,14 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', ()=> {
       assert.equal(window.cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33 ),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     });
+    it ('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+      assert.equal(window.cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33),"abcdefghijklmnopqrstuvwxyz" ) 
+      });
+    it ('debería retornar "0123456789" para "3456789012" con offset 33', () => {
+        assert.equal(window.cipher.decode("3456789012", 33),"0123456789") 
+      });
+    
+
   });
 
 });
